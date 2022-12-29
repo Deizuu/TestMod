58d7fd496c481550e2325d1cb15f52f373d563f1
unknown <ianis.tocaci@gmail.com>
2022-12-29 23:02:20 +0200

    Added Jump Wand & Self Jump Wand

--- src/main/java/deizu/testmod/testmod/core/ModItems.java
+++ src/main/java/deizu/testmod/testmod/core/ModItems.java
@@ -1,11 +1,11 @@
 package deizu.testmod.testmod.core;
 
+import deizu.testmod.testmod.core.customblocks.SelfJumpWandItem;
 import deizu.testmod.testmod.core.customitems.KillGemItem;
 import deizu.testmod.testmod.Testmod;
-import deizu.testmod.testmod.core.customitems.PushWandItem;
+import deizu.testmod.testmod.core.customitems.JumpWandItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -23,9 +23,12 @@ public class ModItems {
     public static final RegistryObject<KillGemItem> KILL_GEM = ITEMS.register("kill_gem", () ->
             new KillGemItem(new Item.Properties().tab(CreativeTab.ITEMS_TAB)));
 
-    public static final RegistryObject<PushWandItem> PUSH_WAND = ITEMS.register("push_wand", () ->
-            new PushWandItem(new Item.Properties().tab(CreativeTab.ITEMS_TAB)));
+    public static final RegistryObject<JumpWandItem> JUMP_WAND = ITEMS.register("jump_wand", () ->
+            new JumpWandItem(new Item.Properties().tab(CreativeTab.ITEMS_TAB).stacksTo(1)));
 
     public static final RegistryObject<Item> JUMP_BLOCK = ITEMS.register("jump_block", () ->
             new BlockItem(ModBlocks.JUMP_BLOCK.get(), new Item.Properties().tab(CreativeTab.BLOCKS_TAB)));
+
+    public static final RegistryObject<Item> SELF_JUMP_WAND = ITEMS.register("self_jump_wand", () ->
+            new SelfJumpWandItem(new Item.Properties().tab(CreativeTab.ITEMS_TAB)));
 }
